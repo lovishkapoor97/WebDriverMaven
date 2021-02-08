e812d03bfcc07e2a11e69d863e3b5d667a024d5e
Lovish <lovishkapoor94@yahoo,in>
2021-02-08 18:00:56 +0530

    My Initial Commit

---
+++ src/test/java/loginTest.java
@@ -0,0 +1,43 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class loginTest {
+	
+	public static WebDriver driver;
+
+	
+	@BeforeSuite
+	public void setup()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver= new FirefoxDriver();
+		
+	}
+	
+	@Test
+	public void LaunchBrowser() throws InterruptedException
+	{
+		
+		driver.get("https://www.gmail.com/");
+		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("lovishkapoor97");
+		driver.findElement(By.xpath("//div[@id='identifierNext']//div[@class='VfPpkd-dgl2Hf-ppHlrf-sM5MNb']//button[@type='button']//div[@class='VfPpkd-RLmnJb']")).click();
+		driver.findElement(By.id("Passwd")).sendKeys("null");
+
+	}
+	
+	@AfterSuite
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+	
+
+}
